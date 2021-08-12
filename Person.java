48528de2c17aeaf7780c028a2278826ab4e87f45
Sukhin <VVS11101992@gmail.com>
2021-08-12 20:45:40 +0300

    Add Person.java

---
+++ Person.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Person {
+    /* Method create object class Scanner. Exit information messages,
+     carries out enter name and surname user, causes method name_surname*/
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scan.nextLine();
+        System.out.print("Enter your surname: ");
+        String surname = scan.nextLine();
+        name_surname(name, surname);
+    }
+    // Create new method for exit the name and the surname user
+    public static void name_surname(String a, String b) {
+        System.out.println(a);
+        System.out.print(b);
+    }
+}
